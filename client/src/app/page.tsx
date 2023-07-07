89b712a11e7b11c65dba130b1d264ba980a9be13
Ankur singh <ankur@rudrainnovative.in>
2023-07-07 18:45:56 +0530

    read operations

---
+++ client/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import { useEffect } from 'react'
+import { ethers } from 'ethers'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return (<div>
+
+
+  </div>)
+}
+
+export default page
